44c35d8c269ccfed44cdb4e29ab416a2ae78b999
Nick Franken <shnick@gmail.com>
2017-03-25 18:02:59 -0500

    updated adapter specs

--- spec/adapters/mysql_adapter_spec.cr
+++ spec/adapters/mysql_adapter_spec.cr
@@ -4,101 +4,102 @@ require "../helper_methods"
 module Crecto
   module Adapters
     module Mysql
-      def self.exec_execute(query_string, params, tx : DB::Transaction?)
+      def exec_execute(conn : DB::Database, query_string, params, tx : DB::Transaction?)
         Crecto::Adapters.sqls << query_string
-        previous_def(query_string, params, tx)
+        previous_def(conn, query_string, params, tx)
       end
 
-      def self.exec_execute(query_string, tx : DB::Transaction?)
+      def exec_execute(conn : DB::Database, query_string, tx : DB::Transaction?)
         Crecto::Adapters.sqls << query_string
-        previous_def(query_string, tx)
+        previous_def(conn, query_string, tx)
       end
 
-      def self.exec_execute(query_string, params)
+      def exec_execute(conn : DB::Database, query_string, params)
         Crecto::Adapters.sqls << query_string
-        previous_def(query_string, params)
+        previous_def(conn, query_string, params)
       end
 
-      def self.exec_execute(query_string)
+      def self.exec_execute(conn, query_string, params : Array)
         Crecto::Adapters.sqls << query_string
-        previous_def(query_string)
+        previous_def(conn, query_string, params)
+      end
+
+      def exec_execute(conn : DB::Database, query_string)
+        Crecto::Adapters.sqls << query_string
+        previous_def(conn, query_string)
       end
     end
   end
 end
 
-if Crecto::Repo::ADAPTER == Crecto::Adapters::Mysql
+describe "Crecto::Adapters::Mysql" do
+  Spec.before_each do
+    Crecto::Adapters.clear_sql
+  end
 
-  describe "Crecto::Adapters::Mysql" do
-    Spec.before_each do
-      Crecto::Adapters.clear_sql
+  it "should generate insert query" do
+    MysqlRepo.insert(User.from_json(%({ "name": "chuck" })))
+    check_sql do |sql|
+      sql.should eq([
+        "INSERT INTO users (name, things, nope, yep, some_date, pageviews, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        "SELECT * FROM users WHERE id = LAST_INSERT_ID()"
+      ])
     end
+  end
 
-    it "should generate insert query" do
-      Crecto::Repo.insert(User.from_json(%({ "name": "chuck" })))
-      check_sql do |sql|
-        sql.should eq([
-          "INSERT INTO users (name, things, nope, yep, some_date, pageviews, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-          "SELECT * FROM users WHERE id = LAST_INSERT_ID()"
-        ])
-      end
+  it "should generate get query" do
+    user = MysqlRepo.insert(User.from_json("{ \"name\":\"lucy\" }"))
+    Crecto::Adapters.clear_sql
+    MysqlRepo.get(User, user.instance.id)
+    check_sql do |sql|
+      sql.should eq(["SELECT * FROM users WHERE id=? LIMIT 1"])
     end
+  end
 
-    it "should generate get query" do
-      user = Crecto::Repo.insert(User.from_json("{ \"name\":\"lucy\" }"))
-      Crecto::Adapters.clear_sql
-      Crecto::Repo.get(User, user.instance.id)
-      check_sql do |sql|
-        sql.should eq(["SELECT * FROM users WHERE id=? LIMIT 1"])
-      end
+  it "should generate sql for query syntax" do
+    query = Query
+      .where(name: "fridge")
+      .where("users.things < ?", [124])
+      .order_by("users.name ASC")
+      .order_by("users.things DESC")
+      .limit(1)
+    MysqlRepo.all(User, query)
+    check_sql do |sql|
+      sql.should eq(["SELECT users.* FROM users WHERE  users.name=? AND users.things < ? ORDER BY users.name ASC, users.things DESC LIMIT 1"])
     end
+  end
 
-    it "should generate sql for query syntax" do
-      query = Crecto::Repo::Query
-        .where(name: "fridge")
-        .where("users.things < ?", [124])
-        .order_by("users.name ASC")
-        .order_by("users.things DESC")
-        .limit(1)
-      Crecto::Repo.all(User, query)
-      check_sql do |sql|
-        sql.should eq(["SELECT users.* FROM users WHERE  users.name=? AND users.things < ? ORDER BY users.name ASC, users.things DESC LIMIT 1"])
-      end
+  it "should generate update queries" do
+    changeset = MysqlRepo.insert(User.from_json(%({ "name": "linus" })))
+    Crecto::Adapters.clear_sql
+    changeset.instance.name = "snoopy"
+    changeset.instance.yep = false
+    MysqlRepo.update(changeset.instance)
+    check_sql do |sql|
+      sql.should eq([
+        "UPDATE users SET name=?, things=?, nope=?, yep=?, some_date=?, pageviews=?, created_at=?, updated_at=? WHERE id=#{changeset.instance.id}",
+        "SELECT * FROM users WHERE id = #{changeset.instance.id}"
+      ])
     end
+  end
 
-    it "should generate update queries" do
-      changeset = Crecto::Repo.insert(User.from_json(%({ "name": "linus" })))
-      Crecto::Adapters.clear_sql
-      changeset.instance.name = "snoopy"
-      changeset.instance.yep = false
-      Crecto::Repo.update(changeset.instance)
-      check_sql do |sql|
-        sql.should eq([
-          "UPDATE users SET name=?, things=?, nope=?, yep=?, some_date=?, pageviews=?, created_at=?, updated_at=? WHERE id=#{changeset.instance.id}",
-          "SELECT * FROM users WHERE id = #{changeset.instance.id}"
-        ])
-      end
+  it "should generate delete queries" do
+    changeset = MysqlRepo.insert(User.from_json(%({ "name": "sally" })))
+    Crecto::Adapters.clear_sql
+    MysqlRepo.delete(changeset.instance)
+    check_sql do |sql|
+      sql.should eq([] of String)
     end
+  end
 
-    it "should generate delete queries" do
-      changeset = Crecto::Repo.insert(User.from_json(%({ "name": "sally" })))
-      Crecto::Adapters.clear_sql
-      Crecto::Repo.delete(changeset.instance)
-      check_sql do |sql|
-        sql.should eq([] of String)
-      end
+  it "should generate IS NULL query" do
+    quick_create_user("nullable")
+    Crecto::Adapters.clear_sql
+    query = Query.where(things: nil)
+    MysqlRepo.all(User, query)
+    check_sql do |sql|
+      sql.should eq(["SELECT users.* FROM users WHERE  users.things IS NULL"])
     end
-
-    it "should generate IS NULL query" do
-      quick_create_user("nullable")
-      Crecto::Adapters.clear_sql
-      query = Crecto::Repo::Query.where(things: nil)
-      Crecto::Repo.all(User, query)
-      check_sql do |sql|
-        sql.should eq(["SELECT users.* FROM users WHERE  users.things IS NULL"])
-      end
-    end
-
   end
 
 end
--- spec/adapters/sqlite3_adapter_spec.cr
+++ spec/adapters/sqlite3_adapter_spec.cr
@@ -4,101 +4,102 @@ require "../helper_methods"
 module Crecto
   module Adapters
     module SQLite3
-      def self.exec_execute(query_string, params, tx : DB::Transaction?)
+      def exec_execute(conn : DB::Database, query_string, params, tx : DB::Transaction?)
         Crecto::Adapters.sqls << query_string
-        previous_def(query_string, params, tx)
+        previous_def(conn, query_string, params, tx)
       end
 
-      def self.exec_execute(query_string, tx : DB::Transaction?)
+      def exec_execute(conn : DB::Database, query_string, tx : DB::Transaction?)
         Crecto::Adapters.sqls << query_string
-        previous_def(query_string, tx)
+        previous_def(conn, query_string, tx)
       end
 
-      def self.exec_execute(query_string, params)
+      def exec_execute(conn : DB::Database, query_string, params)
         Crecto::Adapters.sqls << query_string
-        previous_def(query_string, params)
+        previous_def(conn, query_string, params)
       end
 
-      def self.exec_execute(query_string)
+      def self.exec_execute(conn, query_string, params : Array)
         Crecto::Adapters.sqls << query_string
-        previous_def(query_string)
+        previous_def(conn, query_string, params)
+      end
+
+      def exec_execute(conn : DB::Database, query_string)
+        Crecto::Adapters.sqls << query_string
+        previous_def(conn, query_string)
       end
     end
   end
 end
 
-if Crecto::Repo::ADAPTER == Crecto::Adapters::SQLite3
+describe "Crecto::Adapters::SQLite3" do
+  Spec.before_each do
+    Crecto::Adapters.clear_sql
+  end
 
-  describe "Crecto::Adapters::SQLite3" do
-    Spec.before_each do
-      Crecto::Adapters.clear_sql
+  it "should generate insert query" do
+    u = SQLiteRepo.insert(User.from_json(%({ "name": "chuck", "yep": false })))
+    check_sql do |sql|
+      sql.should eq([
+        "INSERT INTO users (name, things, nope, yep, some_date, pageviews, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        "SELECT * FROM users WHERE id = #{u.instance.id}"
+      ])
     end
+  end
 
-    it "should generate insert query" do
-      u = Crecto::Repo.insert(User.from_json(%({ "name": "chuck", "yep": false })))
-      check_sql do |sql|
-        sql.should eq([
-          "INSERT INTO users (name, things, nope, yep, some_date, pageviews, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-          "SELECT * FROM users WHERE id = #{u.instance.id}"
-        ])
-      end
+  it "should generate get query" do
+    user = SQLiteRepo.insert(User.from_json("{ \"name\":\"lucy\" }"))
+    Crecto::Adapters.clear_sql
+    SQLiteRepo.get(User, user.instance.id)
+    check_sql do |sql|
+      sql.should eq(["SELECT * FROM users WHERE id=? LIMIT 1"])
     end
+  end
 
-    it "should generate get query" do
-      user = Crecto::Repo.insert(User.from_json("{ \"name\":\"lucy\" }"))
-      Crecto::Adapters.clear_sql
-      Crecto::Repo.get(User, user.instance.id)
-      check_sql do |sql|
-        sql.should eq(["SELECT * FROM users WHERE id=? LIMIT 1"])
-      end
+  it "should generate sql for query syntax" do
+    query = Query
+      .where(name: "fridge")
+      .where("users.things < ?", [124])
+      .order_by("users.name ASC")
+      .order_by("users.things DESC")
+      .limit(1)
+    SQLiteRepo.all(User, query)
+    check_sql do |sql|
+      sql.should eq(["SELECT users.* FROM users WHERE  users.name=? AND users.things < ? ORDER BY users.name ASC, users.things DESC LIMIT 1"])
     end
+  end
 
-    it "should generate sql for query syntax" do
-      query = Crecto::Repo::Query
-        .where(name: "fridge")
-        .where("users.things < ?", [124])
-        .order_by("users.name ASC")
-        .order_by("users.things DESC")
-        .limit(1)
-      Crecto::Repo.all(User, query)
-      check_sql do |sql|
-        sql.should eq(["SELECT users.* FROM users WHERE  users.name=? AND users.things < ? ORDER BY users.name ASC, users.things DESC LIMIT 1"])
-      end
+  it "should generate update queries" do
+    changeset = SQLiteRepo.insert(User.from_json(%({ "name": "linus", "yep": true })))
+    Crecto::Adapters.clear_sql
+    changeset.instance.name = "snoopy"
+    changeset.instance.yep = false
+    SQLiteRepo.update(changeset.instance)
+    check_sql do |sql|
+      sql.should eq([
+        "UPDATE users SET name=?, things=?, nope=?, yep=?, some_date=?, pageviews=?, created_at=?, updated_at=? WHERE id=#{changeset.instance.id}",
+        "SELECT * FROM users WHERE id = #{changeset.instance.id}"
+      ])
     end
+  end
 
-    it "should generate update queries" do
-      changeset = Crecto::Repo.insert(User.from_json(%({ "name": "linus", "yep": true })))
-      Crecto::Adapters.clear_sql
-      changeset.instance.name = "snoopy"
-      changeset.instance.yep = false
-      Crecto::Repo.update(changeset.instance)
-      check_sql do |sql|
-        sql.should eq([
-          "UPDATE users SET name=?, things=?, nope=?, yep=?, some_date=?, pageviews=?, created_at=?, updated_at=? WHERE id=#{changeset.instance.id}",
-          "SELECT * FROM users WHERE id = #{changeset.instance.id}"
-        ])
-      end
+  it "should generate delete queries" do
+    changeset = SQLiteRepo.insert(User.from_json(%({ "name": "sally" })))
+    Crecto::Adapters.clear_sql
+    SQLiteRepo.delete(changeset.instance)
+    check_sql do |sql|
+      sql.should eq([] of String)
     end
+  end
 
-    it "should generate delete queries" do
-      changeset = Crecto::Repo.insert(User.from_json(%({ "name": "sally" })))
-      Crecto::Adapters.clear_sql
-      Crecto::Repo.delete(changeset.instance)
-      check_sql do |sql|
-        sql.should eq([] of String)
-      end
+  it "should generate IS NULL query" do
+    quick_create_user("nullable")
+    Crecto::Adapters.clear_sql
+    query = Query.where(things: nil)
+    SQLiteRepo.all(User, query)
+    check_sql do |sql|
+      sql.should eq(["SELECT users.* FROM users WHERE  users.things IS NULL"])
     end
-
-    it "should generate IS NULL query" do
-      quick_create_user("nullable")
-      Crecto::Adapters.clear_sql
-      query = Crecto::Repo::Query.where(things: nil)
-      Crecto::Repo.all(User, query)
-      check_sql do |sql|
-        sql.should eq(["SELECT users.* FROM users WHERE  users.things IS NULL"])
-      end
-    end
-
   end
 
 end
